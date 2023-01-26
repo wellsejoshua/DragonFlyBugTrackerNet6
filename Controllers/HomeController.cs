5eac89db742db9a40b6ec2d65b97c6992b931ea7
Joshua Wells <codedbywells@gmail.com>
2023-01-26 03:35:38 -0600

    Add wwwroot files for UI, fix Home Controller, Fix Home Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,167 @@
 ﻿using DragonFlyBugTrackerNet6.Models;
+using DragonFlyBugTrackerNet6.Models.ChartModels;
+using DragonFlyBugTrackerNet6.Models.Enums;
+using DragonFlyBugTrackerNet6.Models.ViewModels;
+using DragonFlyBugTrackerNet6.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TheBugTracker.Extensions;
 
 namespace DragonFlyBugTrackerNet6.Controllers
 {
-  public class HomeController : Controller
-  {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-      _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly IBTCompanyInfoService _companyInfoService;
+        private readonly IBTProjectService _projectService;
 
-    public IActionResult Index()
-    {
-      return View();
-    }
+        public HomeController(ILogger<HomeController> logger, IBTProjectService projectService, IBTCompanyInfoService companyInfoService)
+        {
+            _logger = logger;
+            _projectService = projectService;
+            _companyInfoService = companyInfoService;
+        }
 
-    public IActionResult Privacy()
-    {
-      return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        public async Task<IActionResult> Dashboard()
+        {
+
+            DashboardViewModel model = new();
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).Where(t => t.Archived == false).ToList();
+            model.Members = model.Company.Members.ToList();
+
+
+            return View(model);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+
+        [HttpPost]
+        public async Task<JsonResult> GglProjectTickets()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+
+            List<object> chartData = new();
+            chartData.Add(new object[] { "ProjectName", "TicketCount" });
+
+            foreach (Project prj in projects)
+            {
+                chartData.Add(new object[] { prj.Name, prj.Tickets.Count() });
+            }
+
+            return Json(chartData);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> GglProjectPriority()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+
+            List<object> chartData = new();
+            chartData.Add(new object[] { "Priority", "Count" });
+
+
+            foreach (string priority in Enum.GetNames(typeof(BTProjectPriority)))
+            {
+                int priorityCount = (await _projectService.GetAllProjectsByPriority(companyId, priority)).Count();
+                chartData.Add(new object[] { priority, priorityCount });
+            }
+
+            return Json(chartData);
+        }
+
+
+        [HttpPost]
+        public async Task<JsonResult> AmCharts()
+        {
+
+            AmChartData amChartData = new();
+            List<AmItem> amItems = new();
+
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+
+            foreach (Project project in projects)
+            {
+                AmItem item = new();
+
+                item.Project = project.Name;
+                item.Tickets = project.Tickets.Count;
+                item.Developers = (await _projectService.GetProjectMembersByRoleAsync(project.Id, nameof(Roles.Developer))).Count();
+
+                amItems.Add(item);
+            }
+
+            amChartData.Data = amItems.ToArray();
+
+
+            return Json(amChartData.Data);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> PlotlyBarChart()
+        {
+            PlotlyBarData plotlyData = new();
+            List<PlotlyBar> barData = new();
+
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+
+            //Bar One
+            PlotlyBar barOne = new()
+            {
+                X = projects.Select(p => p.Name).ToArray(),
+                Y = projects.SelectMany(p => p.Tickets).GroupBy(t => t.ProjectId).Select(g => g.Count()).ToArray(),
+                Name = "Tickets",
+                Type = "bar"
+            };
+
+            //Bar Two
+            PlotlyBar barTwo = new()
+            {
+                X = projects.Select(p => p.Name).ToArray(),
+                Y = projects.Select(async p => (await _projectService.GetProjectMembersByRoleAsync(p.Id, nameof(Roles.Developer))).Count).Select(c => c.Result).ToArray(),
+                Name = "Developers",
+                Type = "bar"
+            };
+
+            barData.Add(barOne);
+            barData.Add(barTwo);
+
+            plotlyData.Data = barData;
+
+            return Json(plotlyData);
+        }
+
+
+
     }
-  }
 }
---
+++ Views/Home/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DragonFlyBugTrackerNet6.Views.Home
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
